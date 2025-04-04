53f59c0d21ae471957089494ce2d375fe1102afd
Maina2982 <johnmaina2982@gmail.com>
2025-04-04 10:32:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,37 +7,6 @@ function getAffiliateCode() {
     localStorage.setItem("affiliate_code", affiliateCode); // Replace with actual code
     return affiliateCode;
 }
-document.addEventListener("DOMContentLoaded", function () {
-    fetch(`${BASE_URL}/tips/wins`)
-        .then(response => response.json())
-        .then(data => {
-            const tableBody = document.getElementById("tips-table");
-            const table = document.getElementById("tipsTable");
-            if (!table.tHead) {
-                const thead = table.createTHead();
-                const headerRow = thead.insertRow();
-                const headers = ["Date", "Match", "Tip", "Odds", "Outcome"];
-                headers.forEach(headerText => {
-                    const th = document.createElement("th");
-                    th.textContent = headerText;
-                    headerRow.appendChild(th);
-                });
-            }
-            tableBody.innerHTML = "";
-            data.forEach(tip => {
-                const row = document.createElement("tr");
-                row.innerHTML = `
-                   <td data-label="date">${tip.date}</td>
-                    <td data-label="match">${tip.match}</td>
-                    <td data-label="tip">${tip.tip}</td>
-                    <td data-label="odds">${tip.odds}</td>
-                    <td data-label="outcome">${tip.outcome}</td>
-                `;
-                tableBody.appendChild(row);
-            });
-        })
-        .catch(error => console.error("Error fetching data:", error));
-});
 // Function to extract affiliate code from URL (assuming format: ?1)
 async function trackClick() {
     const affiliate_code = getAffiliateCode();
